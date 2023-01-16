2fe346c7cb8d8a18f38aaa1bcf724613ed10a783
CocoLng <corentin.lange@hotmail.fr>
2023-01-16 22:05:38 +0100

    Just Connect

---
+++ .github/scripts/Q-learning.py
@@ -0,0 +1,27 @@
+import numpy as np
+import random
+
+# Create a Q-table with the dimensions of the state and action space
+q_table = np.zeros((state_space_size, action_space_size))
+
+# Define the learning rate, discount factor, and exploration rate
+learning_rate = 0.8
+discount_factor = 0.95
+exploration_rate = 0.1
+
+# Define the on_message event handler
+@client.event
+async def on_message(message):
+    # Get the current state of the game
+    current_state = get_current_state()
+    # Choose a random action with a probability of exploration_rate
+    if random.uniform(0, 1) < exploration_rate:
+        action = random.randint(0, action_space_size - 1)
+    else:
+        action = np.argmax(q_table[current_state])
+    # Take the action and get the next state and reward
+    next_state, reward = take_action(action)
+    # Update the Q-table
+    q_table[current_state][action] = (1 - learning_rate) * q_table[current_state][action] + learning_rate * (reward + discount_factor * np.max(q_table[next_state]))
+    # Update the current state
+    current_state = next_state
